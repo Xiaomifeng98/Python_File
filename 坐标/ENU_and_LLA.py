aa2e31ef50d365a390cdcfcd1d3bff9aab394673
Xiaomifeng98 <1138803648@qq.com>
2020-03-26 02:48:19 +0800

    使用了pymap3d的库进行坐标转换

--- 坐标/ENU_and_LLA.py
+++ 坐标/ENU_and_LLA.py
@@ -1,164 +1,97 @@
-# 经纬高坐标系（地理坐标系）：LLA = {'longiude(经度)', 'latitude(纬度)', 'altitude(高度)'}
-# 站心坐标系：ENU
-# 地心地固坐标系：ECEF
-import math
-import easygui as g
+import pymap3d as pm
+import easygui as eg
 
-a = float('%.8f' % 6378137.0)
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+#获取ENU数据
+def get_enu_data(value):
+    if value == 0:
+        inp_msg = eg.multenterbox('请输入以下的数值', '', ['初始E', '初始N', '初始U'])
+    else:
+        inp_msg = eg.multenterbox('请输入以下的数值', '', ['当前E', '当前N', '当前U'])
+    E = float(inp_msg[0])
+    N = float(inp_msg[1])
+    U = float(inp_msg[2])
+    enu_data = [E, N, U]
+    return enu_data
 
+#获取geodetic数据
+def get_geo_data(value):
+    if value == 0:
+        inp_msg = eg.multenterbox('请输入以下的数值', '', ['初始经度', '初始纬度', '初始高度'])
+    else:
+        inp_msg = eg.multenterbox('请输入以下的数值', '', ['当前经度', '当前纬度', '当前高度'])
+    longitude = float(inp_msg[0])
+    latitude = float(inp_msg[1])
+    altitude = float(inp_msg[2])
+    geo_data = [longitude, latitude, altitude]
+    return geo_data
 
-# -------------------------------------------------------------
-# LLA坐标系
-class LLA():
-    # 初始化经纬高三个变量
-    def __init__(self, longiude=0.000, latitude=0.000, altitude=0.000):
-        self.__init_longiude = float('%.8f' % longiude)
-        self.__init_latitude = float('%.8f' % latitude)
-        self.__init_altitude = float('%.3f' % altitude)
+#同时获取ENU和LLA的值-->enu2lla
+def get_enu2lla_data():
+    inp_msg = eg.multenterbox('请输入以下的数值', '', ['当前E', '当前N', '当前U', '初始经度', '初始纬度', '初始高度'])
+    E = float(inp_msg[0])
+    N = float(inp_msg[1])
+    U = float(inp_msg[2])
+    init_longitude = float(inp_msg[3])
+    init_latitude = float(inp_msg[4])
+    init_altitude = float(inp_msg[5])
+    E2G_data = pm.enu2geodetic(E, N, U, init_latitude, init_longitude, init_altitude)
+    return E2G_data
 
-    # 设置全部
-    def set_lla(self, longiude, latitude, altitude, value):
-        if value == 1:
-            self.__longiude = float('%.8f' % longiude)
-            self.__latitude = float('%.8f' % latitude)
-            self.__altitude = float('%.3f' % altitude)
-        else:
-            self.__init_longiude = float('%.8f' % longiude)
-            self.__init_latitude = float('%.8f' % latitude)
-            self.__inti_altitude = float('%.3f' % altitude)
+#同时获取初始LLA和当前LLA的值
+def get_lla2enu_data():
+    inp_msg = eg.multenterbox('请输入以下的数值', '', ['初始经度', '初始纬度', '初始高度', '当前经度', '当前纬度', '当前高度'])
+    init_longitude = float(inp_msg[0])
+    init_latitude = float(inp_msg[1])
+    init_altitude = float(inp_msg[2])
+    current_longitude = float(inp_msg[3])
+    current_latitude = float(inp_msg[4])
+    current_altitude = float(inp_msg[5])
+    G2E_data = pm.geodetic2enu(current_latitude, current_longitude, current_altitude, init_latitude, init_longitude, init_altitude)
+    return G2E_data
 
-    def change(self):
-        h1 = float(self.__init_altitude + a)
-        la1 = float(self.__init_latitude / 180 * math.pi)
-        lo1 = float(self.__init_longiude / 180 * math.pi)
-        h2 = float(self.__altitude + a)
-        la2 = float(self.__latitude / 180 * math.pi)
-        lo2 = float(self.__longiude / 180 * math.pi)
-        x1 = float(h1 * math.cos(la1) * math.cos(lo1))
-        y1 = float(h1 * math.cos(la1) * math.sin(lo1))
-        z1 = float(h1 * math.sin(la1))
-        x2 = float(h2 * math.cos(la2) * math.cos(lo2))
-        y2 = float(h2 * math.cos(la2) * math.sin(lo2))
-        z2 = float(h2 * math.sin(la2))
-        Dis = float((((x1 - x2) ** 2) + ((y1 - y2) ** 2) + ((z1 - z2) ** 2)) ** 0.5)
-        change_longiude = self.__longiude - self.__init_longiude
-        change_latitude = self.__latitude - self.__init_latitude
-        change_altitude = self.__altitude - self.__init_altitude
-        change_data = {'Dis' : Dis, 'EW' : abs(x2 - x1), 'NS' : abs(y2 - y1), 'UD' : abs(z2 - z1), 'change_longiude': change_longiude, 'change_latitude': change_latitude, 'change_altitude': change_altitude}
-        return change_data
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+def E2G_function():
+    E2G_data = get_enu2lla_data()
+    current_geo = 'longitude: ' + str(float('%.8f' % E2G_data[1])) + '\nlatitude: ' + str(float('%.8f' % E2G_data[0])) + '\naltitude: ' + str(float('%.3f' % E2G_data[2]))
+    eg.msgbox(current_geo, '当前的经纬高坐标为')
 
-    # LLA转换成ENU
-    def LLA2ENU(self):
-        h1 = float(self.__init_altitude + a)
-        la1 = float(self.__init_latitude / 180 * math.pi)
-        lo1 = float(self.__init_longiude / 180 * math.pi)
-        h2 = float(self.__altitude + a)
-        la2 = float(self.__latitude / 180 * math.pi)
-        lo2 = float(self.__longiude / 180 * math.pi)
-        x1 = float(h1 * math.cos(la1) * math.cos(lo1))
-        y1 = float(h1 * math.cos(la1) * math.sin(lo1))
-        z1 = float(h1 * math.sin(la1))
-        x2 = float(h2 * math.cos(la2) * math.cos(lo2))
-        y2 = float(h2 * math.cos(la2) * math.sin(lo2))
-        z2 = float(h2 * math.sin(la2))
-        Dis = float((((x1 - x2) ** 2) + ((y1 - y2) ** 2) + ((z1 - z2) ** 2)) ** 0.5)
-        change_longiude = self.__longiude - self.__init_longiude
-        change_latitude = self.__latitude - self.__init_latitude
-        change_altitude = self.__altitude - self.__init_altitude
-        e = float(a * math.cos(self.__latitude / 180 * math.pi) * (self.__longiude - self.__init_longiude))
-        n = float(a * (self.__latitude - self.__init_latitude))
-        u = float(self.__altitude - self.__init_altitude)
-        data_enu = {'Dis': float('%.3f' % Dis), 'EW': float('%.3f' % abs(x2 - x1)), 'NS': float('%.3f' % abs(y2 - y1)), 'UD': float('%.3f' % abs(z2 - z1)),
-                       'change_longiude': float('%.8f' % change_longiude), 'change_latitude': float('%.8f' % change_latitude),
-                       'change_altitude': float('%.3f' % change_altitude), 'E': float('%.3f' % e), 'N': float('%.3f' % n), 'U': float('%.3f' % u)}
-        return data_enu
+def G2E_function():
+    G2E_data  =get_lla2enu_data()
+    current_enu = 'E: ' + str(float('%.3f' % G2E_data[0])) + '\nN: ' + str(float('%.3f' % G2E_data[1])) + '\nU: ' + str(float('%.3f' % G2E_data[2]))
+    eg.msgbox(current_enu, '当前的ENU坐标为')
 
+def EEGG_function():
+    init_geo = get_geo_data(0)
+    fir_enu = get_enu_data(0)
+    sec_enu = get_enu_data(1)
+    sec_geo = pm.enu2geodetic(fir_enu[0], fir_enu[1], fir_enu[2], init_geo[1], init_geo[0], init_geo[2])
+    thi_geo = pm.enu2geodetic(sec_enu[0], sec_enu[0], sec_enu[0], sec_geo[1], sec_geo[0], sec_geo[2])
+    msg = '当前经度：' + str(float('%.8f' % thi_geo[1])) + '\n当前纬度：' + str(float('%.8f' % thi_geo[0])) + '\n当前高度：' + str(float('%.8f' % thi_geo[2]))
+    eg.msgbox(msg, '结果')
 
-# -------------------------------------------------------------
-class ENU():
-    # 经纬高坐标设置为ENU坐标原点
-    def __init__(self, east=0, north=0, up=0):
-        self.__init_east = float(east)
-        self.__init_north = float(north)
-        self.__init_up = float(up)
+def GGGD_function():
+    G2E_data = get_lla2enu_data()
+    ch_e = float('%.3f' % G2E_data[0])
+    ch_n = float('%.3f' % G2E_data[1])
+    ch_u = float('%.3f' % G2E_data[2])
+    ch_L = float('%.3f' % ((ch_e ** 2) + (ch_n ** 2) + (ch_u ** 2)) ** 0.5)
+    msg = '直线距离：' + str(ch_L) + '\n东西距离：' + str(ch_e) + '\n南北距离：' + str(ch_n) + '\n上下距离：' + str(ch_u)
+    eg.msgbox(msg, '结果')
+    
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
-    # 设置全部
-    def set_enu(self, east, north, up, value):
-        if value == 1:
-            self.__east = float(east)
-            self.__north = float(north)
-            self.__up = float(up)
-        else:
-            self.__init_east = float(east)
-            self.__init_north = float(north)
-            self.__init_up = float(up)
-    def change(self):
-        change_east = self.__east - self.__init_east
-        change_north = self.__north - self.__init_north
-        change_up = self.__up - self.__init_up
-        change_data = {'change_east' : float('%.3f' % change_east), 'change_north' : float('%.3f' % change_north), 'change_up' : float('%.3f' % change_up)}
-        return change_data
-
-    def ENU2LLA(self, init_longiude, init_latitude, init_altitude):
-        altitude = float('%.3f' % (init_altitude + self.__up))
-        latitude = float('%.3f' % ((self.__north + init_latitude) / a))
-        longiude = float('%.3f' % ((self.__east + init_longiude) / (a * math.cos(latitude / 180 * math.pi))))
-        data_lla = {'经度': longiude, '纬度': latitude, '高度': altitude}
-        return data_lla
-
-
-# -------------------------------------------------------------
 while 1:
-    inputmsg_init = g.multenterbox('初始化起点经纬高度', '请输入', ['经度', '纬度', '高度'])
-    # 初始化的经纬高度
-    init_longiude = float(inputmsg_init[0])
-    init_latitude = float(inputmsg_init[1])
-    init_altitude = float(inputmsg_init[2])
-    lla = LLA(init_longiude, init_latitude, init_altitude)
-    #初始化的ENU
-    inputmsg_init = g.multenterbox('初始化起点ENU', '请输入', ['E', 'N', 'U'])
-    init_e = float(inputmsg_init[0])
-    init_n = float(inputmsg_init[1])
-    init_u = float(inputmsg_init[2])
-    # 建立对象
-    enu = ENU(init_e, init_n, init_u)
-    while 1:
-        choice = g.buttonbox('请选择您要执行的操作：', choices=('LLA --> ENU', 'ENU --> LLA', '重新输入初始经纬高度', '退出'))
-        if choice == 'LLA --> ENU':
-            inputmsg = g.multenterbox('当前经纬高度', '请输入', ['经度', '纬度', '高度'])
-            longiude = float(inputmsg[0])
-            latitude = float(inputmsg[1])
-            altitude = float(inputmsg[2])
-            lla.set_lla(longiude, latitude, altitude, 1)  # 设置当前经纬高度
-            l2e = lla.LLA2ENU()
-            l2e_1 = '变化：\n' + \
-                    '  直线距离：' + str(l2e['Dis']) + \
-                    '\n  东西距离：' + str(l2e['EW']) + \
-                    '\n  南北距离：' + str(l2e['NS']) + \
-                    '\n  上下距离：' + str(l2e['UD']) + \
-                    '\n当前ENU：\n' + \
-                    '  E: ' + str(l2e['E']) + '\n  N: ' + str(l2e['N']) + '\n  U: ' + str(l2e['U'])
-            g.msgbox(l2e_1, 'ENU结果为')
-        elif choice == 'ENU --> LLA':
-            inputmsg = g.multenterbox('当前ENU坐标', '请输入', ['E', 'N', 'U'])
-            e = float(inputmsg[0])
-            n = float(inputmsg[1])
-            u = float(inputmsg[2])
-            enu.set_enu(e, n, u, 1) #设置当前ENU坐标
-            enu_change = enu.change()
-            e2l = enu.ENU2LLA(init_longiude, init_latitude, init_altitude)
-            e2l_1 = '变化：\n' +\
-                    '  E: ' + str(enu_change['change_east']) + \
-                    '\n  N:' + str(enu_change['change_north']) + \
-                    '\n  U:' + str(enu_change['change_up']) + '\n' + \
-                    '当前：\n' + \
-                    '  经度：' + str(e2l['经度']) + \
-                    '\n  纬度：' + str(e2l['纬度']) + \
-                    '\n  高度：' + str(e2l['高度'])
-            g.msgbox(e2l_1, '结果为')
-        elif choice == '根据经纬高度计算距离':
-            g.msgbox('根据经纬高度计算距离')
-        elif choice == '重新输入初始经纬高度':
-            break
-        else:
-            exit(0)
+    fun_msg_cho = eg.buttonbox('请选择你要执行的操作', choices = ('ENU -> Geodetic', 'Geodetic -> ENU', '根据两个ENU计算\n新位置的经纬高度', '根据两个经纬高度\n计算相关的距离', '退出'))
+    if fun_msg_cho == 'ENU -> Geodetic':
+        E2G_function()
+    elif fun_msg_cho == 'Geodetic -> ENU':
+        G2E_function()
+    elif fun_msg_cho == '根据两个ENU计算\n新位置的经纬高度':
+        EEGG_function()
+    elif fun_msg_cho == '根据两个经纬高度\n计算相关的距离':
+        GGGD_function()
+    else:
+        break
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------
